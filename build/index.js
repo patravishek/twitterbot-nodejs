5e719294187db3440197cd64c4a8e928975f260e
Avishek Patra <avishek.patra@outlook.com>
2020-03-20 18:18:20 +0530

    Configuration of typescript project.

---
+++ build/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class HelloWorld {
+    constructor() {
+        console.log("Hello World");
+    }
+    connectTwitter() {
+        return "hello life";
+    }
+}
+exports.HelloWorld = HelloWorld;
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export class HelloWorld{
+    constructor(){
+        console.log("Hello World");
+    }
+
+    connectTwitter(): any {
+        return "hello life";
+    }
+}
