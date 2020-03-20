a53be6236d8d21cb08f6d82c48bcd54dde46e003
Avishek Patra <avishek.patra@outlook.com>
2020-03-20 19:23:46 +0530

    changes and pulling the tweets

--- build/index.js
+++ build/index.js
@@ -1,11 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class HelloWorld {
-    constructor() {
-        console.log("Hello World");
-    }
-    connectTwitter() {
-        return "hello life";
-    }
-}
-exports.HelloWorld = HelloWorld;
+const ts_twitter_1 = require("ts-twitter");
+require("dotenv").config();
+let twitter = new ts_twitter_1.Twitter(process.env.TWITTER_CONSUMER_KEY, process.env.TWITTER_CONSUMER_SECRET);
+twitter.getUserTimeline({ screen_name: "gbico" })
+    .then(tweets => {
+    console.info(tweets);
+});
+twitter.searchTweets({ q: "@gbico" })
+    .then(function (t) {
+    console.info(t);
+})
+    .catch(function (e) {
+    console.info(e);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-export class HelloWorld{
-    constructor(){
-        console.log("Hello World");
-    }
+import { Twitter } from "ts-twitter";
+require("dotenv").config();
 
-    connectTwitter(): any {
-        return "hello life";
-    }
-}
+let twitter = new Twitter(
+  process.env.TWITTER_CONSUMER_KEY,
+  process.env.TWITTER_CONSUMER_SECRET,
+  process.env.TOKEN,
+  process.env.TOKEN_SECRET
+);
+ 
+twitter.getUserTimeline({ screen_name: "gbico" })
+  .then(tweets => {
+    console.info(tweets);
+});
+ 
+twitter.searchTweets({ q: "@gbico" })
+    .then(function (t) {
+    console.info(t);
+})
+    .catch(function (e) {
+    console.info(e);
+});
